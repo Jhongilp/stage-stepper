580a477d97272bc03e25ada59bf8dac53ab4c5b7
Jonathan Gil <jhongilp@gmail.com>
2021-02-01 19:22:46 -0500

    add values for checkpoint

--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -1,38 +1,42 @@
-import React, {FC} from "react";
-import { StepperProps, Step, CheckpointProps } from '../types';
-import './_stepper.scss';
+import React, { FC } from "react";
+import { StepperProps, Step, CheckpointProps } from "../types";
+import CheckpointCircularProgress from './checkpoint/Checkpoint';
+import "./_stepper.scss";
 
-const Checkpoint:FC<CheckpointProps> = ({position, size, barHeight}) => {
+const Checkpoint: FC<CheckpointProps> = ({ position, size, barHeight }) => {
   return (
-    <div className="st-progress-bar-checkpoint" 
-      style={
-        {
-          left: `calc(${position}% - ${size / 2}px)`, 
-          top: `${-((size/2)-(barHeight/2))}px`, 
-          // top: `${size / 2}px - ${barHeight / 2}px)`, 
-          width: size, 
-          height: size 
-        }
-      }
-    >
-    </div>
-  )
-}
+    <CheckpointCircularProgress progress={50} position={position} size={size} barHeight={barHeight}/>
+    // <div
+    //   className="st-progress-bar-checkpoint"
+    //   style={{
+    //     left: `calc(${position}% - ${size / 2}px)`,
+    //     top: `${-(size / 2 - barHeight / 2)}px`,
+    //     width: size,
+    //     height: size,
+    //   }}
+    // ></div>
+  );
+};
 
-export const Stepper: FC<StepperProps> = ({steps, height}) => {
+export const Stepper: FC<StepperProps> = ({ steps, height }) => {
   return (
     <div className="st-wrapper">
-      <div className="st-progress_bar-container" style={{height}}>
-        {
-          steps.map((step, i) => {
-            const offset = 10; // 0 - 100, treat as %, 2 sides (left and right)  
-            const gap = (100 - (offset * 2)) / (steps.length - 1) ;
-            const stepPosition = (gap * i) + (offset);
-            return <Checkpoint position={stepPosition} size={step.size} barHeight={height}/>
-          })
-        }
+      <div className="st-progress_bar-container" style={{ height }}>
+        {steps.map((step, i) => {
+          const offset = 10; // 0 - 100, treat as %, 2 sides (left and right)
+          const gap = (100 - offset * 2) / (steps.length - 1);
+          const stepPosition = gap * i + offset;
+          return (
+            <Checkpoint
+              position={stepPosition}
+              size={step.size}
+              barHeight={height}
+            />
+          );
+        })}
         <div className="st-progress_bar-progress"></div>
       </div>
+      {/* <CheckpointTest /> */}
     </div>
   );
 };
---
+++ src/components/checkpoint/Checkpoint.tsx
@@ -0,0 +1,81 @@
+import React, { FC, useState, useRef, useEffect, ReactNode } from "react";
+import { CheckpointProps } from '../../types'
+
+type CircularProgressProps = {
+  progress: number;
+};
+
+const CheckpointCircularProgress: FC<CircularProgressProps & CheckpointProps> = ({
+  progress,
+  position,
+  size,
+  barHeight
+}) => {
+// const CheckpointCircularProgress = () => {
+  // const [progress, setProgress] = useState(35);
+  const [circumference, setCircumference] = useState(0);
+  const [cssStyle, setStyle] = useState({
+    strokeDasharray: `${0} ${0}`,
+    strokeDashoffset: `${0}`,
+  });
+
+  const circleRef = useRef<SVGCircleElement>(null);
+
+  useEffect(() => {
+    const radio = circleRef?.current?.r?.baseVal?.value;
+    if (radio) {
+      const circumference = radio * 2 * Math.PI;
+      console.log("[mount] radio value: ", radio);
+      console.log("[mount] circumference value: ", circumference);
+      const offset = circumference - progress / 100 * circumference;
+      const style = {
+        strokeDasharray: `${circumference} ${circumference}`,
+        strokeDashoffset: `${offset}`,
+        // strokeDashoffset: `${circumference}`,
+      };
+      setCircumference(circumference)
+      setStyle(style);
+    }
+  }, [circleRef.current]);
+
+  useEffect(() => {
+    // const offset = circumference - parseInt(e.target.value) / 100 * circumference;
+    const offset = circumference - progress / 100 * circumference;
+    console.log("offset: ", offset)
+    console.log("on change circumference: ", circumference)
+    const style = {
+      strokeDasharray: `${circumference} ${circumference}`,
+      strokeDashoffset: `${offset}`,
+    };
+    setStyle(style);
+  }, [progress])
+
+  return (
+    <div 
+      // className="svg-wrapper-test" 
+      className="st-progress-bar-checkpoint" 
+      style={{
+        left: `calc(${position}% - ${size / 2}px)`,
+        top: `${-(size / 2 - barHeight / 2)}px`,
+        width: size,
+        height: size,
+      }}
+    >
+      <svg className="progress-ring" viewBox="0 0 24 24" width="100%" height="100%">
+        <circle
+          ref={circleRef}
+          className="progress-ring__circle"
+          stroke="green"
+          strokeWidth="2"
+          fill="white"
+          r="11"
+          cx="12"
+          cy="12"
+          style={cssStyle}
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default CheckpointCircularProgress;
